bf3f70cb8e86464812ca4f0fb040e830b6453caf
nonokangwei <weikang@cisco.com>
2018-11-19 18:34:31 +0800

    Update brigade.js

--- brigade.js
+++ brigade.js
@@ -1,5 +1,10 @@
 const { events } = require("brigadier");
 
 events.on("push", function(e, project) {
-  console.log("received push for commit " + e.revision.commit)
+  var job = new Job("print", "alpine:3.4")
+  job.tasks = [
+    "echo revision " + e.revision.commit
+  ]
+
+  job.run()
 })
